02d4d57871e603acad90cc1e0dd7adc118fd38a1
yonin7 <yehonisan@gmail.com>
2023-01-28 04:09:06 +0200

    add style for app,header container, page Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Board from "./Components/Board";
+import { AppContainer, Header, Title } from "./AppStyles";
 
 
 function App() {
   return (
-    <div className="App">
-
+    <AppContainer >
+      <Header><Title>TIC-TAC-TOE</Title></Header>
       <Board  />
-    </div>
+    </AppContainer>
   );
 }
 
